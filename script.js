e64c6b4bdc711d3d51e936a6f6f3c3ddfc589af7
Jackson <jacksonconnors@hotmail.com>
2025-08-08 13:45:22 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -1,73 +1,126 @@
 document.getElementById("submitButton").addEventListener("click", function () {
-  const rawInput = document.getElementById("userInput").value;
+  const rawData = document.getElementById("userInput").value;
+  const ics_events = genorateEvents(rawData);
+  const ics_file = wrapEvents(ics_events);
+  console.log(ics_file);
+  // genorateDownloadLink(ics_file);
+});
+
+function genorateEvents(rawData) {
   const CHUNK_LENGTH = 13
   const TITLE_LINE = 0;
+  const INSTRUCTOR_LINE = 4;
+  const START_LINE_OFFSET = 2;
   const DATA_TABLE_LINE = 11;
-  var finding_courses = true
-  var lines = rawInput.split('\n');
+  var lines = rawData.split('\n');
+  var parsingData = true
   var events = "";
-  while (finding_courses) {
+  while (parsingData) {
     var start_index = -1;
     for (var i = 0; i < lines.length; i++) {
+      // A course data chunk has been located
       if (lines[i].indexOf('CRN') !== -1) {
-        start_index = i - 2;
+        start_index = i - START_LINE_OFFSET;
         break;
       } 
     }
     if (start_index == -1) {
-        finding_courses = false;
+      // No more course data chunks found
+        parsingData = false;
         break;
     }
+    // Cut off any prior data
     lines.splice(0, start_index);
-    const courseInfo = parseData(lines[DATA_TABLE_LINE], lines[TITLE_LINE]);
-    const icsContent = generateICSFromCourse(courseInfo);
+    if (lines[DATA_TABLE_LINE].indexOf("TBA") == -1) {
+      const event = genorateEvent(lines[DATA_TABLE_LINE], lines[TITLE_LINE], lines[INSTRUCTOR_LINE]);
+      const icsContent = formatEvent(event);
+      events += icsContent;
+    }
+    // Remove the processed data chunk
     lines = lines.splice(0 + CHUNK_LENGTH - 1);
-    events += icsContent;
   }
-  const ics_file = encloseICSForm(events);
-  console.log(ics_file);
-  const blob = new Blob([ics_file], { type: 'text/calendar' });
-  const link = document.createElement('a');
-  link.href = URL.createObjectURL(blob);
-  link.download = 'calendar.ics';
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-});
+  return events;
+}
 
-function parseData(rawData, title) {
-  const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  var firstDigitIndex = rawData.indexOf(rawData.match(/\d/));
-  var lastTimeIndex = rawData.lastIndexOf(":") + 6
-  var classTimes = rawData.substring(firstDigitIndex, lastTimeIndex).split(' - ');
-  var startTime = classTimes[0].trim();
-  var endTime = classTimes[1].trim();
-  var classDays = rawData.match(/[MTWRF]+/g);
+function genorateEvent(rawData, title_line, instructor_line) {
+  const LAST_TIME_DELTA = 6
+  const MONTHS = [
+    'Jan', 'Feb', 'Mar', 'Apr', 
+    'May', 'Jun', 'Jul', 'Aug', 
+    'Sep', 'Oct', 'Nov', 'Dec'
+  ];
+  const firstDigitIndex = rawData.indexOf(rawData.match(/\d/));
+  const lastTimeIndex = rawData.lastIndexOf(":") + LAST_TIME_DELTA;
+  const classTimes = rawData.substring(firstDigitIndex, lastTimeIndex).split(' - ');
+  const classDays = rawData.match(/[MTWRF]+/g);
+  var instructor = instructor_line
+  if (instructor.indexOf('E-mail') !== -1) {
+    instructor = instructor.substring(0, instructor.indexOf('E-mail')).trim();
+  }
+  if (instructor.indexOf(':') !== -1) {
+    instructor = instructor.substring(instructor.indexOf(':') + 1).trim();
+  }
   rawData = rawData.substring(lastTimeIndex + classDays[0].length + 1).trim();
   var firstMonthIndex = 99999;
   for (var month of MONTHS) {
-      var monthIndex = rawData.indexOf(month);
-      if (monthIndex !== -1 && monthIndex < firstMonthIndex) {
-          firstMonthIndex = monthIndex;
-      }
+    var monthIndex = rawData.indexOf(month);
+    if (monthIndex !== -1 && monthIndex < firstMonthIndex) {
+        firstMonthIndex = monthIndex;
+    }
   }
-  var location = rawData.substring(0, firstMonthIndex).trim();
-  var classDates = rawData.substring(firstMonthIndex, rawData.lastIndexOf('2025') + 4).split(' - ');
-  var startDate = classDates[0].trim();
-  var endDate = classDates[1].trim();
-  var courseInfo = {
-      title: title,
-      startTime: startTime,
-      endTime: endTime,
-      days: classDays[0].split(''),
-      location: location,
-      startDate: startDate,
-      endDate: endDate
+  const location = rawData.substring(0, firstMonthIndex).trim();
+  const classDates = rawData.substring(firstMonthIndex, rawData.lastIndexOf('2025') + 4).split(' - ');
+  const startDate = classDates[0].trim();
+  const endDate = classDates[1].trim();
+  const startTime = classTimes[0].trim();
+  const endTime = classTimes[1].trim();
+  const event = {
+    title: title_line,
+    startTime: startTime,
+    endTime: endTime,
+    days: classDays[0].split(''),
+    location: location,
+    startDate: startDate,
+    endDate: endDate,
+    instructor: instructor
   };
-  return courseInfo;
+  return event;
 }
 
-function formatICSDate(date, time, UTC) {
+function formatEvent(event) {
+  const uid = generateUID()
+  const title = event.title;
+  const location = event.location;
+  const startDate = event.startDate
+  // endDate must be in UTC
+  const endDate = formatDate(event.endDate, event.endTime, true);
+  const startTime = formatDate(startDate, event.startTime, false);
+  const endTime = formatDate(startDate, event.endTime, false);
+  const byDay = event.days.map(d => ({ M: 'MO', T: 'TU', W: 'WE', R: 'TH', F: 'FR', S: 'SA', U: 'SU' }[d])).join(',');
+  const instructor = event.instructor;
+
+  const icsContent = `
+  BEGIN:VEVENT
+  UID:${uid}
+  DTSTAMP:20240101T000000Z
+  SUMMARY:${title}
+  DESCRIPTION:Scheduled Class with ${instructor}
+  LOCATION:${location}
+  DTSTART;TZID=America/New_York:${startTime}
+  DTEND;TZID=America/New_York:${endTime}
+  RRULE:FREQ=WEEKLY;BYDAY=${byDay};UNTIL=${endDate}Z
+  STATUS:CONFIRMED
+  SEQUENCE:0
+  TRANSP:OPAQUE
+  BEGIN:VALARM
+  TRIGGER:-PT15M
+  ACTION:DISPLAY
+  END:VALARM
+  END:VEVENT`;
+  return icsContent;
+}
+
+function formatDate(date, time, UTC) {
   const monthMap = {
     Jan: '01', Feb: '02', Mar: '03', Apr: '04',
     May: '05', Jun: '06', Jul: '07', Aug: '08',
@@ -92,23 +145,16 @@ function formatICSDate(date, time, UTC) {
   return `${formattedDate}T${formattedTime}`;
 }
 
-function generateUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  }) + '@northeastern.edu';
-}
-
-function encloseICSForm(content) {
-  var warapped_content = `
+function wrapEvents(events) {
+  var wrapped_events = `
   BEGIN:VCALENDAR
   VERSION:2.0
-  PRODID:-//Class Generator//EN
+  PRODID:-//CalendarGenerator//EN
   CALSCALE:GREGORIAN
-  ${content}
-  
+  ${events}
+
   END:VCALENDAR`;
-  var lines = warapped_content.split('\n');
+  var lines = wrapped_events.split('\n');
   var cleaned_content = '';
   for (var line of lines) {
     cleaned_content += line.trim() + '\n';
@@ -116,34 +162,19 @@ function encloseICSForm(content) {
   return cleaned_content
 }
 
-function generateICSFromCourse(courseInfo) {
-    const uid = generateUID()
-    const title = courseInfo.title || '';
-    const location = courseInfo.location || '';
-    const startDate = courseInfo.startDate
-    const endDate = formatICSDate(courseInfo.endDate, courseInfo.endTime, true);
-    const startTime = formatICSDate(startDate, courseInfo.startTime, false);
-    const endTime = formatICSDate(startDate, courseInfo.endTime, false);
-    const byDay = courseInfo.days.map(d => ({ M: 'MO', T: 'TU', W: 'WE', R: 'TH', F: 'FR', S: 'SA', U: 'SU' }[d])).join(',');
-
-    const icsContent = `
-    BEGIN:VEVENT
-    UID:${uid}
-    DTSTAMP:20240101T000000Z
-    SUMMARY:${title}
-    DESCRIPTION:Scheduled Class
-    LOCATION:${location}
-    DTSTART;TZID=America/New_York:${startTime}
-    DTEND;TZID=America/New_York:${endTime}
-    RRULE:FREQ=WEEKLY;BYDAY=${byDay};UNTIL=${endDate}Z
-    STATUS:CONFIRMED
-    SEQUENCE:0
-    TRANSP:OPAQUE
-    BEGIN:VALARM
-    TRIGGER:-PT15M
-    ACTION:DISPLAY
-    END:VALARM
-    END:VEVENT`;
-    return icsContent;
+function generateUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  }) + '@northeastern.edu';
 }
 
+function genorateDownloadLink(ics_file) {
+  const blob = new Blob([ics_file], { type: 'text/calendar' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = 'calendar.ics';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
